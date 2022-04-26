d671f0aa52301e12502e12934eb61cc4b0a57288
EmmanuelliTalaska <manutalaska@gmail.com>
2022-04-25 21:33:45 -0300

    projeto sons de animais

---
+++ js/script.js
@@ -0,0 +1,95 @@
+const som1 = document.getElementById('m1')
+const som2 = document.getElementById('m2')
+const som3 = document.getElementById('m3')
+const som4 = document.getElementById('m4')
+const som5 = document.getElementById('m5')
+const som6 = document.getElementById('m6')
+const som7 = document.getElementById('m7')
+const som8 = document.getElementById('m8')
+
+const img1 = document.getElementById('im1')
+const img2 = document.getElementById('im2')
+const img3 = document.getElementById('im3')
+const img4 = document.getElementById('im4')
+const img5 = document.getElementById('im5')
+const img6 = document.getElementById('im6')
+const img7 = document.getElementById('im7')
+const img8 = document.getElementById('im8')
+
+img1.addEventListener('click', ()=>{
+    som1.src = "./som/avestruz.mp3"
+    m1.play();
+})
+img1.addEventListener('dblclick', ()=>{
+    m1.pause();
+})
+
+
+
+img2.addEventListener('click', ()=>{
+    som2.src = "./som/elefante.mp3"
+    m2.play();
+})
+img2.addEventListener('dblclick', ()=>{
+    m2.pause();
+})
+
+
+
+img3.addEventListener('click', ()=>{
+    som3.src = "./som/girafa.mp3"
+    m3.play();
+})
+img3.addEventListener('dblclick', ()=>{
+    m3.pause();
+})
+
+
+
+img4.addEventListener('click', ()=>{
+    som3.src = "./som/hipopotamo.mp3"
+    m4.play();
+})
+img4.addEventListener('dblclick', ()=>{
+    m4.pause();
+})
+
+
+
+img5.addEventListener('click', ()=>{
+    som5.src = "./som/leao.mp3"
+    m5.play();
+})
+img5.addEventListener('dblclick', ()=>{
+    m5.pause();
+})
+
+
+
+img6.addEventListener('click', ()=>{
+    som6.src = "./som/macaco.mp3"
+    m6.play();
+})
+img6.addEventListener('dblclick', ()=>{
+    m6.pause();
+})
+
+
+
+img7.addEventListener('click', ()=>{
+    som7.src = "./som/tucano.mp3"
+    m7.play();
+})
+img7.addEventListener('dblclick', ()=>{
+    m7.pause();
+})
+
+
+
+img8.addEventListener('click', ()=>{
+    som8.src = "./som/zebra.mp3"
+    m8.play();
+})
+img8.addEventListener('dblclick', ()=>{
+    m8.pause();
+})
